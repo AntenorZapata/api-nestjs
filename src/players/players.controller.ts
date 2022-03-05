7eec473e6589749fe5fc5acb11769613e3815a2e
Antenor <antenorzapata@gmail.com>
2022-03-05 18:28:19 -0300

    feat: create Player services

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { CreatePlayerDto } from './dtos/create-player.dto'
+import { PlayersService } from './players.service'
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(readonly playersService: PlayersService) {}
   @Post()
-  createPlayer(@Body() createPlayerDto: CreatePlayerDto) {
-    const { name } = createPlayerDto
-    return name
+  create(@Body() createPlayerDto: CreatePlayerDto) {
+    return this.playersService.create(createPlayerDto)
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+import { CreatePlayerDto } from './dtos/create-player.dto'
+import { Player } from './interfaces/player.interface'
+import { v4 as uuid } from 'uuid'
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private players: Player[] = []
+
+  private readonly logger = new Logger()
+  async create(player: CreatePlayerDto): Promise<void> {
+    this.storeOnArray(player)
+  }
+
+  private storeOnArray(player: CreatePlayerDto): void {
+    const { name, phoneNumber, email } = player
+
+    const newPlayer: Player = {
+      _id: uuid(),
+      name,
+      phoneNumber,
+      email,
+      ranking: 'ranking1',
+      rankingPosition: 10,
+      photoUrl: 'url_da_foto',
+    }
+
+    this.logger.log(this.players)
+
+    this.players.push(newPlayer)
+    console.log(this.players)
+  }
+}
